dec98776a6a619f9cbe2fafc56d92c178c3257f1
wenowenoweno <113591590+wenowenoweno@users.noreply.github.com>
2022-10-18 23:10:48 +0200

    Create fichero7.php

---
+++ files/fichero7.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+        <h1>Operaciones Sistemas Ficheros</h1>
+        <form>
+            Fichero Origen (Path/nombre): <input type="text" name="origen"><br><br>
+            Fichero Destino (Path/nombre): <input type="text" name="destino"><br>
+            <br><br>
+            Operaciones: <br>
+            <label><input type="radio" name="op" value="copiar"> Copiar fichero<br></label>
+            <label><input type="radio" name="op" value="renombrar"> Renombrar fichero<br></label>
+            <label><input type="radio" name="op" value="borrar"> Borrar fichero<br></label>
+            
+            <br><br>
+            <input type="submit" value="Ejecutar Operacion">
+            <input type="reset" value="Borrar">
+            <br><br>
+        </form>
+        <h1>Operaciones Sistemas Ficheros</h1>
+    </body>
+</html>
