2d81ce52da60dc29a58d11c3fa014c98620d5769
wenowenoweno <113591590+wenowenoweno@users.noreply.github.com>
2022-10-21 13:17:36 +0200

    Update ej1s.php

--- strings/ej1s.php
+++ strings/ej1s.php
@@ -4,8 +4,12 @@
 <?php
 $ip="192.18.16.204";
 $num=$ip;$ip1=substr($ip,0,strpos($ip,'.'));
-$ip=substr($ip,strpos($ip,'.')+1);$ip2=substr($ip,0,strpos($ip,'.'));
-$ip=substr($ip,strpos($ip,'.')+1);$ip3=substr($ip,0,strpos($ip,'.'));
-$ip=substr($ip,strpos($ip,'.')+1);$ip4=substr($ip,0);printf("Numero $num se representa en binario como %08b.%08b.%08b.%08b <br/>",$ip1,$ip2,$ip3,$ip4);?>
+$ip=substr($ip,strpos($ip,'.')+1);
+$ip2=substr($ip,0,strpos($ip,'.'));
+$ip=substr($ip,strpos($ip,'.')+1);
+$ip3=substr($ip,0,strpos($ip,'.'));
+$ip=substr($ip,strpos($ip,'.')+1);
+$ip4=substr($ip,0);
+printf("Numero $num se representa en binario como %08b.%08b.%08b.%08b <br/>",$ip1,$ip2,$ip3,$ip4);?>
 </BODY>
 </HTML>
