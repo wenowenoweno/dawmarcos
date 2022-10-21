701f72fe9f2692dfd813fbd5ad9af96b929d4bda
wenowenoweno <113591590+wenowenoweno@users.noreply.github.com>
2022-10-21 10:37:02 +0200

    Update funciones_bolsa.php

--- bolsa/funciones_bolsa.php
+++ bolsa/funciones_bolsa.php
@@ -1,4 +1,13 @@
 <?php
+
+function validar($datos){
+
+    $datos=trim($datos);
+    $datos=stripslashes($datos);
+    $datos=htmlspecialchars($datos);
+    return $datos;
+}
+
 function mostrarDatos($url){
     $file=file($url);
     foreach($file as $linea){
@@ -6,6 +15,23 @@ function mostrarDatos($url){
         echo($linea);
         echo"</pre>";
         echo"<br>";
+    }
+}
+
+function introducirDatos($nombre){
+    $file=file("ibex35.txt");
+    $nombre=strtoupper(validar($_POST["nombre"]));
+    $test=0;
+    for($x=1;$x<count($file);$x++){
+        $linea=$file[$x];
+        $nombrelist=substr($linea, 0, strpos($linea, " "));
+        if($nombre==$nombrelist){
+            echo "<pre>".$linea."</pre>";
+            $test++;
+        }
+    }
+    if($test==0){
+        echo "Nombre no encontrado";
     }
 }
 ?>
