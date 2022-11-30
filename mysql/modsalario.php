0150e852de566c2d5d14e958b507c73130a8bfd5
wenowenoweno <113591590+wenowenoweno@users.noreply.github.com>
2022-11-30 12:31:44 +0100

    Update modsalario.php

--- mysql/modsalario.php
+++ mysql/modsalario.php
@@ -1,7 +1,7 @@
 <html>
     <body>
         <h1>Modificar salario</h1>
-        <form action="modsalario.php" method="get">
+        <form action="modsalario.php" method="POST">
             Selecciona un dni:  
             <?php
             require "fun.php";
@@ -9,39 +9,44 @@
             selectDNI($conn);
             $conn=null;
             ?>
-            <input type="submit" value="Ver sueldo">
+            <input type="submit" value="Ver sueldo" name="b1">
+        </form>
             Salario actual:
             <?php
-                 if($_SERVER["REQUEST_METHOD"]=="GET"){ 
-                    try {
-                        $conn=conexion();
-                        $dni=$_GET["dni"];
-                        mostrarSueldo($conn, $dni);
-                        $conn=null;
+                 if($_SERVER["REQUEST_METHOD"]=="POST"){ 
+
+                    if($_POST!=null){
+                        if(isset($_POST["b1"])){
+                            try {
+                                $conn=conexion();
+                                $dni=$_POST["dni"];
+                                mostrarSueldo($conn, $dni);
+                                $conn=null;
+                                }
+                            catch(PDOException $e) {
+                                    echo "Error: " . $e->getMessage();
+                                }
                         }
-                    catch(PDOException $e) {
-                            echo "Error: " . $e->getMessage();
+                        if(isset($_POST["b2"])){
+                            try {
+                                $conn=conexion();
+                                $sueldo=$_POST["nuevo"];
+                                cambiarSueldo($conn, $sueldo, $dni);
+                                $conn=null;  
+                            }
+                            catch(PDOException $e) {
+                                    echo "Error: " . $e->getMessage();
+                            }      
                         }
-                $conn=null;
+                        
+                    }
                }           
             ?>
-        </form>
-        <form action="modsalario.php" method="post">
-            Nuevo sueldo: <input type="text" name="nuevo"><br><br>
-            <input type="submit" value="Cambiar sueldo">
-        </form>
+            <br><br>
+            <form method="POST" action="modsalario.php">
+                Nuevo sueldo: <input type="text" name="nuevo"><br><br>
+                <input type="submit" value="Cambiar sueldo" name="b2">
+            </form>
     </body>
 </html>
-<?php
-    if($_SERVER["REQUEST_METHOD"]=="POST"){ 
-        try {
-            $conn=conexion();
-            $sueldo=$_POST["nuevo"];
-            cambiarSueldo($conn, $sueldo);
-            $conn=null;
-        }
-        catch(PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
-    }
-?>
+
